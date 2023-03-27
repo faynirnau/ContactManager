7aa6d5b1418f8ec78d6f3b471ea30eeb6cc37f7d
naude <naudet.jeremy@laposte.net>
2023-03-27 13:22:19 +0200

    La BDD fonctionne

--- app/src/main/java/com/example/contactmanager/controler/MainActivity.java
+++ app/src/main/java/com/example/contactmanager/controler/MainActivity.java
@@ -2,20 +2,20 @@ package com.example.contactmanager.controler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-
 import com.example.contactmanager.R;
+import com.example.contactmanager.modele.AdapterRecycleV;
+import com.example.contactmanager.modele.Bdd;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView contactList;
-    RecyclerView.Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contactList.findViewById(R.id.recycleViewContacts);
-        contactList.setAdapter(adapter);
-        System.out.println("helo");
+        Bdd bdd = new Bdd();
+        bdd.execute();
+        contactList = findViewById(R.id.recycleViewContacts);
+        contactList.setAdapter(new AdapterRecycleV());
     }
 }
--- app/src/main/java/com/example/contactmanager/modele/Bdd.java
+++ app/src/main/java/com/example/contactmanager/modele/Bdd.java
@@ -1,10 +1,61 @@
 package com.example.contactmanager.modele;
 
 import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.util.Log;
 
-public class Bdd extends AsyncTask {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Bdd extends AsyncTask<String, Void, String> {
+    String res;
     @Override
-    protected Object doInBackground(Object[] objects) {
-        return null;
+    protected String doInBackground(String... strings) {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+        ResultSetMetaData rsmd = null;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://10.0.2.2:3306/contactmanager", "root", "root");
+            String result = "Database Connection Successful\n";
+            Log.d("BDD", "doInBackground: Connection Reussite");
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT lastname, firstname, email, phonenumber FROM contact;");
+            rsmd = rs.getMetaData();
+
+            while (rs.next()) {
+                result += rs.getString(1).toString() + " " + rs.getString(2).toString() + " " + rs.getString(3).toString() + "\n";
+            }
+            res = result;
+            Log.d("BDD", "doInBackground: " + res);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ignored) {
+                } // ignore
+
+                rs = null;
+            }
+
+            if (st != null) {
+                try {
+                    st.close();
+                } catch (SQLException ignored) {
+                } // ignore
+
+                st = null;
+            }
+        }
+        return res;
     }
 }
